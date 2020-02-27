8783bbee8f71b6bf21894a60fdfc396989dc485b
trueaddict <50746660+trueaddict@users.noreply.github.com>
2020-02-27 21:19:14 +0200

    ver1
    Organise files to directories by file format

--- README.md
+++
@@ -1,2 +0,0 @@
-# organiser
- Python file organiser
--- other/organiser.py
+++ other/organiser.py
@@ -18,7 +18,7 @@ file_formats = {file_format: directory
 def organize():
     for file1 in os.scandir():
         if file1.is_dir():
-            continue
+            organize_dir()
         file_path = Path(file1.name)
         file_format = file_path.suffix.lower()
         if file_format in file_formats:
@@ -32,12 +32,27 @@ def organize():
         for dir in os.scandir():
             try:
                 if dir.is_dir():
-                    continue
+                    pass
                 else:
                     os.rename(os.getcwd() + '/' + str(Path(dir)), os.getcwd() + '/other/' + str(Path(dir)))
 
             except:
                 pass
+
+def organize_dir():
+    for file2 in os.scandir():
+        if file2.is_dir():
+            continue
+        file_path = Path(file2.name)
+        file_format = file_path.suffix.lower()
+        if file_format in file_formats:
+            directory_path = Path(file_formats[file_format])
+            file_path.rename(directory_path.parent.joinpath(file_path))
+        try:
+            os.mkdir("other")
+        except:
+            pass
+
 
 if __name__ == "__main__":
         organize()
